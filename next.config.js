69c4d9505b98cb7f783489f956e1875fe4ea7ce3
Hasan Bala <balahasan.35@gmail.com>
2022-05-23 16:41:09 +0300

    fonts api updated

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@ module.exports = {
   images: {
     domains: ["raw.githubusercontent.com"],
   },
+  optimizeFonts: false,
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,8 +5,14 @@ class MyDocument extends Document {
     return (
       <Html lang='tr'>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap'
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossorigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&family=Roboto:wght@500&display=swap'
             rel='stylesheet'
           />
           <link
